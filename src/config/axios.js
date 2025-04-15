a4d9a9866c9e322a8d1d8009eea4dbdee25313bd
prdy14 <118160201+prdy14@users.noreply.github.com>
2025-04-15 16:39:15 +0530

    commited

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const instance = axios.create({
-  // baseURL: "https://cricscorer-api-1.onrender.com/api/",
-  baseURL: "http://localhost:8081/api/",
+  baseURL: "https://cricscorer-api-1.onrender.com/api/",
+  // baseURL: "http://localhost:8081/api/",
 });
 
 // Add an interceptor to include the token in requests
